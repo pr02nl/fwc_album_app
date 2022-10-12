d817001a5ab54f20346b70fc68e0446c0b127ab7
Paulo Roberto <pr02nl@gmail.com>
2022-10-12 20:14:58 -0300

    homepage init

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/core/ui/styles/button_styles.dart';
+import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
+import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fwc_album_app/app/pages/home/widgets/status_tile.dart';
+import 'package:fwc_album_app/app/pages/home/widgets/sticker_percent_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,19 +12,95 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: context.colors.primary,
       appBar: AppBar(
-        title: const Text("Home Page"),
+        backgroundColor: context.colors.primary,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+          )
+        ],
       ),
-      body: Center(
-        child: Button.primary(
-          label: "Logout",
-          onPressed: () async {
-            final sp = await SharedPreferences.getInstance();
-            sp.clear();
-            Navigator.of(context)
-                .pushNamedAndRemoveUntil("/", (route) => false);
-          },
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.png"),
+            fit: BoxFit.cover,
+          ),
         ),
+        child: LayoutBuilder(builder: (context, constrains) {
+          return ConstrainedBox(
+            constraints: BoxConstraints(minHeight: constrains.maxHeight),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 35),
+                      width: MediaQuery.of(context).size.width,
+                      child: Image.asset(
+                        "assets/images/bola.png",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const StickerPercentWidget(
+                      percent: 45,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "45 figurinhas",
+                      style: context.textStyles.titleWhite,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    StatusTile(
+                      icon: Image.asset("assets/images/all_icon.png"),
+                      label: "Todas",
+                      value: 34,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    StatusTile(
+                      icon: Image.asset("assets/images/missing_icon.png"),
+                      label: "Faltando",
+                      value: 500,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    StatusTile(
+                      icon: Image.asset("assets/images/repeated_icon.png"),
+                      label: "Repetidas",
+                      value: 30,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Button(
+                      onPressed: () {},
+                      width: MediaQuery.of(context).size.width * .9,
+                      outline: true,
+                      style: context.buttonStyles.yellowOutlineButton,
+                      labelStyle: context.textStyles.textSecondaryFontExtraBold
+                          .copyWith(
+                        color: context.colors.yellow,
+                      ),
+                      label: "Minhas figurinhas",
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        }),
       ),
     );
   }
