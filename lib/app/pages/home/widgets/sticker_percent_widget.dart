a9e3f1ef6f72668972ed19a71943f35569de8e99
Paulo Roberto <pr02nl@gmail.com>
2022-10-12 20:15:31 -0300

    inicio sticker percent

---
+++ lib/app/pages/home/widgets/sticker_percent_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
+import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
+
+class StickerPercentWidget extends StatelessWidget {
+  final int percent;
+  const StickerPercentWidget({super.key, required this.percent});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          width: 70,
+          height: 70,
+          decoration: BoxDecoration(
+            color: context.colors.grey,
+            shape: BoxShape.circle,
+          ),
+          child: Center(
+            child: Text(
+              "$percent",
+              style: context.textStyles.titlePrimaryColor,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
